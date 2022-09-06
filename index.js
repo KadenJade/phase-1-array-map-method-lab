8559eb1b6cd16f81015ef2f26ae6bd64ba940cbf
Kaden <kadengraybill@gmail.com>
2022-09-06 09:03:33 -0500

    commit tree f94ae2e59151771bb920dc5d3b889f91514e9ab3

--- index.js
+++ index.js
@@ -11,6 +11,52 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+ 
+  return tutorials.map((line) => {
+    const newArray = []
+    const tokens = line.split(" ");
+    const arrayLineNowUpper = tokens.map((word) => {
+    const upper = word[0].toUpperCase() + word.slice(1)
+    newArray.push(upper)
+    })     
+    
+   return newArray.join(" ")
+  })
 }
+
+titleCased()
+
+
+
+
+/*
+let newArray = [];
+const upperCaseThat = () => {
+  return tutorials.map(array) => {
+    const arrayString = array.split(" ");
+    let nowCapitalized = capitalize(arrayString[i])
+    let nowSpaced = nowCapitalized.join(" ")
+    newArray.push(nowSpaced)
+  }
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+string.map(capitalize)
+*/
+/*
+//break into strings
+function titleCased(){
+  const noSpace = tutorials.split(" ")
+  //cap each first letter
+  noSpace.map(capitalize)
+  const nowSpaced = noSpace.join(" ")
+  newArray.push(nowSpaced)
+  return newArray
+}
+*/
